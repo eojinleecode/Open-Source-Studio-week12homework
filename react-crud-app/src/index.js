b5faa19e48ca72b54039984126d8e91b26e52695
eojinleecode <2002eo567@handong.ac.kr>
2025-11-24 23:57:29 +0900

    s

--- react-crud-app/src/index.js
+++ react-crud-app/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ShowList from "./components/Pages/ShowList";
-
-const myComponent = <ShowList />;
+import ShowList from "./components/Pages/ShowList";   // 🔥 경로 주의! 슬래시(/) 사용
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(myComponent);
+root.render(
+  <React.StrictMode>
+    <ShowList />
+  </React.StrictMode>
+);
